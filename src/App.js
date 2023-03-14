5467ec1dc083a6c8bb378ea741e2b2664f227ce4
Gothman24 <paez.federico96@gmail.com>
2023-03-13 23:10:16 -0300

    formularios y useState

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react';
+import Formulario from './components/Formularios';
+
+class App extends Fragment {
+  render() {
+    return (
+      <Fragment>
+      <h1>Administrador de Pacientes</h1>
+        <div className='container'>
+          <div className='one-half column'>
+
+          </div>
+          <div className='one-half column'>
+
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
